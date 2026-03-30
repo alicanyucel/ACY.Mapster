e4ae5508f7c2a6c76efda0a55a97ec7d28fc4070
Ali Can Yücel <yucelalican@hotmail.com>
2026-03-30 20:21:16 +0300

    Add project files.

---
+++ ACY.Mapster/Mapping.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+
+namespace ACY.MapsterPattern;
+
+public static class SimpleMapper
+{
+
+    public static TDestination Map<TSource, TDestination>(TSource source)
+        where TDestination : new()
+    {
+        if (source == null) throw new ArgumentNullException(nameof(source));
+
+        var destination = new TDestination();
+        var sourceProps = typeof(TSource).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var destProps = typeof(TDestination).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var sProp in sourceProps)
+        {
+            var dProp = destProps.FirstOrDefault(p => p.Name == sProp.Name && p.PropertyType == sProp.PropertyType);
+            if (dProp != null && dProp.CanWrite)
+            {
+                var value = sProp.GetValue(source);
+                dProp.SetValue(destination, value);
+            }
+        }
+
+        return destination;
+    }
+}
